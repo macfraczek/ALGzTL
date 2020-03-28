1fda13867c823fc837a8e9aadb135c90c75cc6fe
Maciej <macfraczek@gmail.com>
2020-03-28 13:25:20 +0100

    Initial

---
+++ ALGzTL/ModularArithmetic.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ALGzTL
+{
+
+    public class ModularArithmetic
+    {
+        bool _countInfo;
+        public ModularArithmetic(bool countInfo = false)
+        {
+            _countInfo = countInfo;
+        }
+
+        /// <summary>
+        /// solve congurence equations. a = b (mod n)
+        /// </summary>
+        /// <param name="input">exaple '2x = 1 (mod 22)'</param>
+        /// <returns></returns>
+        public int ComputeCongruenceMinimalResult(string input)
+        {
+            var congurence = ParseCongruenceInput(input);
+
+            int x = -1;
+            int moduloCounter = 0;
+            long leftside;
+
+            do
+            {
+                x++;
+
+                leftside = (long)congurence.a * x;
+
+                if (leftside > (1 + moduloCounter) * congurence.n)
+                    moduloCounter++;
+
+            } while ((leftside - moduloCounter * congurence.n) != congurence.b);
+
+            if(_countInfo)
+                Console.WriteLine($"modulo was subtracted {moduloCounter} times");
+            
+            return x;
+        }
+
+        private CongurenceValues ParseCongruenceInput(string input)
+        {
+            if (!input.Contains('x')) throw new Exception($"Not found '=' in '${input}'");
+            var partsByEqualitySign = input.Split('=');
+
+            if (!partsByEqualitySign[0].Contains('x')) throw new Exception($"Not found 'x' in '${input}'");
+            var a = partsByEqualitySign[0].Replace("x", "").Replace("*", "").Replace(" ", "");
+
+            if (!partsByEqualitySign[1].Contains("m")) throw new Exception($"Not found 'm' in '${input}'");
+            var partsByMod = partsByEqualitySign[1].Trim().Split("m");
+
+            var b = partsByMod[0].Replace("(", "").Trim();
+            var n = partsByMod[1].Replace(")", "").Replace("od", "").Trim();
+
+            return new CongurenceValues(a, b, n);
+        }
+    }
+
+    struct CongurenceValues
+    {
+        public int a;
+        public int b;
+        public int n;
+
+        public CongurenceValues(string a, string b, string n)
+        {
+            this.a = int.Parse(a);
+            this.b = int.Parse(b);
+            this.n = int.Parse(n);
+        }
+
+        public override string ToString()
+        {
+            return $"{a}x = {b} (mod {n})";
+        }
+    }
+}
---
+++ ALGzTL/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ALGzTL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           new Program().CongruenceCalculation();
+
+        }
+
+        string[] positiveAnswer = { "y", "yes", "t", "tak" };
+
+        void CongruenceCalculation()
+        {
+            Console.Write("Czy chcesz otrzymac dodatkowe informacje o obliczeniach? ");
+            string userInput = Console.ReadLine().ToLower();
+            bool isAdditionalInfo = positiveAnswer.Any(s => s == userInput); 
+
+            
+            ModularArithmetic ma = new ModularArithmetic(isAdditionalInfo);
+
+            while (true)
+            {
+                Console.WriteLine("Podaj kongruencje. Przykład: '49x = 1 mod 55'");
+                userInput = Console.ReadLine().ToLower();
+
+                Console.WriteLine(ma.ComputeCongruenceMinimalResult(userInput));
+
+                Console.WriteLine("Czy chcesz obliczyc kolejne? ");
+                userInput = Console.ReadLine().ToLower();
+                bool doExit = !positiveAnswer.Any(s => s == userInput);
+                if (doExit) break;
+            }
+        }
+
+    }
+}
